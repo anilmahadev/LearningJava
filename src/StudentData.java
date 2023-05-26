8d35bf0e6ca91c019b4ac53b5df940f046a7ffa8
Anil Mahadev <anilmahadev@outlook.com>
2023-05-25 19:31:44 -0500

    StudentData.java - Primitive Types

---
+++ src/StudentData.java
@@ -0,0 +1,24 @@
+//Primitive Types
+
+public class StudentData {
+
+    public static void main(String[] args) {
+
+// Define datatype
+
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        char studentFirstInitial = 'A';
+        char studentLastInitial = 'M';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+
+    }
+
+
+}
