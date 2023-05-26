2a601fab97161d6160032303f9741e746ca3fafb
Anil Mahadev <anilmahadev@outlook.com>
2023-05-25 19:40:45 -0500

    StudentData.java - Primitive Types - Renamed

---
+++ src/StudentDataRefTypes.java
@@ -0,0 +1,28 @@
+//Working with Reference Types
+
+public class StudentDataRefTypes {
+
+    public static void main(String[] args) {
+
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        char studentFirstInitial = 'A';
+        char studentLastInitial = 'M';
+        boolean hasPerfectAttendance = true;
+
+        //Reference Types mentioned below of type String
+
+        String studentFirstName = "Anchor";
+        String studentLastName = "man";
+
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
+
+    }
+}
